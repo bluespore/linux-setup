73ae27bfb0a7183b34e7b86afe0bfa5aae871f22
Sean Bullock <sean@bluespore.com>
2021-06-11 17:29:45 +1000

    lolcat

--- setup-thinkpad-battery.sh
+++ setup-thinkpad-battery.sh
@@ -2,9 +2,9 @@
 
 figlet "Laptop battery" | lolcat -a -d 2
 
-echo "Installing autocpu-freq..."
+echo "Installing autocpu-freq..." | lolcat
 sudo snap install auto-cpufreq
 sudo auto-cpufreq --install
 echo "Installed." | lolcat 
-echo "Battery optimisations will now be automatically made via the auto-cpufreq daemon."
-echo "For more information: https://github.com/AdnanHodzic/auto-cpufreq"
+echo "Battery optimisations will now be automatically made via the auto-cpufreq daemon." | lolcat
+echo "For more information: https://github.com/AdnanHodzic/auto-cpufreq" | lolcat
