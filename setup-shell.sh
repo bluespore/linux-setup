f3d7ae89342279f885ee0b69cd6f6a047f02bcd8
Sean Bullock <sean@bluespore.com>
2021-06-11 17:29:25 +1000

    lolcat

--- setup-shell.sh
+++ setup-shell.sh
@@ -4,43 +4,43 @@ figlet "Setup shell" | lolcat -a -d 2
 
 echo "Remember to set your default shell to /bin/zsh in Konsole!" | lolcat
 
-echo "Installing build-essential, curl, file & git"
+echo "Installing build-essential, curl, file & git" | lolcat
 sudo apt-get install build-essential curl file git
 
-echo "Installing zsh and ohmyzsh..."
+echo "Installing zsh and ohmyzsh..." | lolcat
 sudo apt install zsh
 chsh -s $(which zsh)
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
-echo "Installing homebrew..."
+echo "Installing homebrew..." | lolcat
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 echo 'eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)' >> /home/x19/.zprofile
 eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 echo 'PATH="$PATH:/home/linuxbrew/.linuxbrew/bin"' >> ~/.zshrc
 source ~/.zshrc
 
-echo "Installing fzf..."
+echo "Installing fzf..." | lolcat
 brew install fzf
 $(brew --prefix)/opt/fzf/install
 source ~/.zshrc
 
-echo "Installing zoxide..."
+echo "Installing zoxide..." | lolcat
 brew install zoxide
 echo 'eval "$(zoxide init zsh)"' >> ~/.zshrc
 source ~/.zshrc
 
-echo "Installing ripgrep..."
+echo "Installing ripgrep..." | lolcat
 sudo apt-get install ripgrep
 
-echo "Installing gcc..."
+echo "Installing gcc..." | lolcat
 brew install gcc
 
-echo "Installing neofetch..."
+echo "Installing neofetch..." | lolcat
 sudo apt install neofetch
 
-echo "Installing xclip..."
+echo "Installing xclip..." | lolcat
 sudo apt-get install -y xclip
 echo 'alias pbcopy="xclip -sel clip"' >> ~/.zshrc
 source ~/.zshrc
-echo "Alias 'pbcopy' set to mimic functionality with xclip"
+echo "Alias 'pbcopy' set to mimic functionality with xclip" | lolcat
 
