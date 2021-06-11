e64ebfab77616c1a8ccbab3a48892eb2915023d4
Sean Bullock <sean@bluespore.com>
2021-06-11 17:28:24 +1000

    lolcat

--- setup-logitech-mouse.sh
+++ setup-logitech-mouse.sh
@@ -2,11 +2,11 @@
 
 figlet "Configure MX Master" | lolcat -a -d 2
 
-echo "Directory .logiops will be created in home"
+echo "Directory .logiops will be created in home" | lolcat
 mkdir -p ~/.logiops
 git clone git@github.com:PixlOne/logiops.git ~/.logiops
 $(cd ~/.logiops && mkdir build && cmake .. && make && sudo make install && sudo systemctl enable logid)
 
 echo "Logiops has been installed" | lolcat
-echo "Configuration found in /etc/logid.cfg"
-echo "Github: https://github.com/PixlOne/logiops"
+echo "Configuration found in /etc/logid.cfg" | lolcat
+echo "Github: https://github.com/PixlOne/logiops" | lolcat
