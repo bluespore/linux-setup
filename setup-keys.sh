edbbbba3d1e3e639d91e1e313841a3f2da4f7bf6
Sean Bullock <sean@bluespore.com>
2021-06-11 17:27:50 +1000

    lolcat

--- setup-keys.sh
+++ setup-keys.sh
@@ -2,9 +2,7 @@
 
 figlet "Setup keys" | lolcat -a -d 2
 
-echo "ssh-keygen running, follow the prompts and use no password if you prefer"
+echo "ssh-keygen running, follow the prompts and use no password if you prefer" | lolcat
 ssh-keygen -t rsa
 echo "Key generated at ~/.ssh/id_rsa.pub" | lolcat
-echo "Browser will open with key in clipboard, so you can add it to your Github account"
-pbcopy < ~/.ssh/id_rsa.pub
-firefox https://github.com/settings/keys
+echo "Use pbcopy < ~/.ssh/id_rsa.pub to clipboard your key when you need to use retrieve it."
